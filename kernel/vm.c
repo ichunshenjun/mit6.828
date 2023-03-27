1870e629b146e78b63e6fa0e3d215d2a327e6d0e
Kezheng Zhang <846009097@qq.com>
2023-03-27 20:55:36 +0800

    完成Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable,int depth){
+  if(depth>2){
+    return;
+  }
+  if(depth==0){
+    printf("page table %p\n",pagetable);
+  }
+  char *delim = 0;
+  if (depth == 0) delim = "..";
+  if (depth == 1) delim = ".. ..";
+  if (depth == 2) delim = ".. .. ..";
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf("%s%d: pte %p pa %p\n", delim, i, pte, PTE2PA(pte));
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      vmprint((pagetable_t)child,depth+1);
+    }
+  }
+}
