3e011519d449d623a9d44047cf2c3e7fbac848f1
Kezheng Zhang <846009097@qq.com>
2023-04-03 16:21:11 +0800

    完成Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -52,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -117,7 +119,9 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  for(int i=0;i<=NBUCKET-1;i++){
+    pthread_mutex_init(&lock[i],NULL);
+  }
   //
   // first the puts
   //
